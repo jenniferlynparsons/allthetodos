50668f4ea7bc517a38e2c3a720e514f5427c3b2f
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2017-03-11 14:21:28 -0500

    woooo. let's restart this thing!
    new plan: JS/OOP vs. Clojure/FP. GO!

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/todo.jar clojure.main -m todo.server
--- env/prod/cljs/todo/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns todo.prod
-  (:require [todo.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
